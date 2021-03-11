589e1aa2dc9550e04616f12dd2e902b7fd71ae4f
Julian Med <julian.medina7@hotmail.com.ar>
2021-03-11 18:50:00 -0300

    Agregando el componente NavBar al app.js

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const NavBar = () => {
+    return(
+        <div>
+            <h3>TODO AMOR</h3>
+            <nav>
+                <ul>
+                    <li><a href="">SOBRE NOSOTROS</a></li>
+                    <li><a href="">COMO COMPRAR</a></li>
+                    <li><a href="">LIBRERIA</a></li>
+                    <li><a href="">ACCESORIOS</a></li>
+                    <li><a href="">COCINA</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+
+}
+
+export default NavBar;
