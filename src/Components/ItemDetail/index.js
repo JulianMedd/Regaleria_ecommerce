6d85140a1d60ad9d50ceb3efda975a75d016e148
Julian Med <julian.medina7@hotmail.com.ar>
2021-04-06 17:32:51 -0300

    desafio sincronizar counter

--- src/Components/ItemDetail/index.js
+++ src/Components/ItemDetail/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{ useState } from "react";
+import ItemCount from "../ItemCount";
+import { Link } from "react-router-dom";
 
 export const ItemDetail = ({itemD}) => {
+    const [count, setCount]=useState(0)
+
+    const agregar = (contador) =>{
+        console.log(contador);
+        setCount(contador)
+    }
+
     return (
         <div>
             
             <h3>item detail</h3>
             <h3>{itemD?.title}</h3>
             <img src={itemD?.image}/>
+            {count === 0 ? 
+                <ItemCount initial={0} stock={5} onAdd={agregar}></ItemCount>
+                :
+                <Link to="/cart">
+                    <button>TERMINAR COMPRA</button>
+                </Link>}
         </div>
     )
 }
